da74635fb39d46f2ce17c8dd0df0b542da1e4927
Bret Gourdie <AdjectiveNounIsAdjective@gmail.com>
2017-10-29 23:39:03 -0400

    Initial try at SoundFileInfo

--- vitas-soundboard/SoundFileInfo.cs
+++ vitas-soundboard/SoundFileInfo.cs
@@ -4,9 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.IO;
+
 namespace vitas_soundboard
 {
     public class SoundFileInfo
     {
+        private DirectoryInfo directory;
+        private string wavExtension = ".wav";
+
+        public SoundFileInfo(string path)
+        {
+            directory = new DirectoryInfo(path);
+
+            if (!directory.Exists)
+            {
+                Console.WriteLine("SoundFileInfo: Directory \"" + path + "\" does not exist!");
+            }
+        }
+
+        public FileInfo[] GetFiles()
+        {
+            return directory.GetFiles("*" + wavExtension);
+        }
     }
 }
