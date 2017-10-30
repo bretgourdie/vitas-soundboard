99338d7a2e1c2981cd9ee314c5c275ba35ded897
Bret Gourdie <AdjectiveNounIsAdjective@gmail.com>
2017-10-29 23:52:56 -0400

    Implemented button creation and clicking

--- vitas-soundboard/VitasSoundboardForm.cs
+++ vitas-soundboard/VitasSoundboardForm.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
 
 namespace vitas_soundboard
 {
     public partial class VitasSoundboardForm : Form
     {
+        private string initialPath = "wavs";
+        private SoundPlayer soundPlayer = new SoundPlayer();
+
         public VitasSoundboardForm()
         {
             InitializeComponent();
+            displaySounds(initialPath);
+        }
+
+        private void displaySounds(string directory)
+        {
+            var sfi = new SoundFileInfo(directory);
+
+            var files = sfi.GetFiles();
+
+            Array.Sort(files);
+
+            foreach(var file in files)
+            {
+                mainTabPageFlowLayoutPanel.Controls.Add(
+                    getButton(file.Name, file.FullName));
+            }
+        }
+
+        private Button getButton(string label, string file)
+        {
+            var button = new Button();
+            button.Text = label;
+            button.Tag = file;
+            button.Click += Button_Click;
+            return button;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            var clickedButton = (Button)sender;
+            soundPlayer.SoundLocation = (string)clickedButton.Tag;
+            soundPlayer.Play();
         }
     }
 }
